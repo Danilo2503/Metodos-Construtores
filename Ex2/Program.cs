c8253a15c7b8e75a8561c654a77ee413cb6588e0
unknown <dannilorch@gmail.com>
2020-12-15 16:49:27 -0300

    Exercicio 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ex2.classes;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto();
+
+            produto1.Codigo = 1;
+            produto1.Nome = "Coca Cola";
+            produto1.Descricao = "Refrigerante de Cola";
+            produto1.Estoque = 30;
+
+            Console.WriteLine($"Produto 1 - Código {produto1.Codigo}, Nome {produto1.Nome}, Descrição {produto1.Descricao}, Estoque {produto1.Estoque}");
+
+            Produto produto2 = new Produto(10);
+            produto2.Nome = "Pepsi";
+
+            Console.WriteLine($"Código inserido {produto2.Codigo}, Nome {produto2.Nome}"); 
+
+            Produto produto3 = new Produto(1,"Traquinas", "Bolacha de chocolate", 1000);
+
+            Console.WriteLine($"Código {produto3.Codigo}, Nome {produto3.Nome}, Descrição {produto3.Descricao}, Estoque {produto3.Estoque}");
+        }
+    }
+}
---
+++ Ex2/classes/Produto.cs
@@ -0,0 +1,21 @@
+namespace Ex2.classes
+{
+    public class Produto
+    {
+        public int Codigo {get; set;}
+        public string Nome {get; set;}
+        public string Descricao {get; set;}
+        public int Estoque {get; set;}
+    }
+
+    public Produto(int valor){
+        Codigo = valor;  
+    }
+
+    public Produto(int codigo, string nome, string descricao, int estoque){
+        Codigo = codigo;
+        Nome = nome;
+        Descricao = descricao;
+        Estoque = estoque;
+    }
+}
