9ec12105ea92fbadd762689210df3210579ed8fb
unknown <dannilorch@gmail.com>
2020-12-15 15:05:36 -0300

    Exercicio 1

---
+++ Ex1/classes/Pessoa.cs
@@ -0,0 +1,23 @@
+namespace Ex1.classes
+{
+    public class Pessoa
+    {
+        private string sobreNome;
+        public string SobreNome{
+            get {return sobreNome;}
+            set {sobreNome = value;}
+        }
+
+        private string nome;
+        public string Nome{
+            get {return nome;} 
+            set {nome = value;}
+        }  
+
+        
+        public Pessoa(string firstName, string lastName){
+            sobreNome = lastName;
+            nome = firstName;
+        }
+    }
+}
